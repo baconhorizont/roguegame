0a03938f1717c181c934d3cffd0c63cbf17ac425
Bence Szabo <bboard16@gmail.com>
2023-12-01 09:22:54 +0100

    test vscode settings

---
+++ game/src/GameEngine.cpp
@@ -0,0 +1,10 @@
+#include "GameEngine.hpp"
+#include <iostream>
+
+namespace game
+{
+    void GameEngine::run()
+    {
+        std::cout << "Hello Bence" << std::endl;
+    }
+}
---
+++ game/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "GameEngine.hpp"
+
+int main(){
+    using namespace game;
+    GameEngine game;
+    game.run();
+    return 0;
+};
